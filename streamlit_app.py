b19e305d523e9ba9ff89824f87998ecd0f5f988c
Goyo <goyodiaz@gmail.com>
2025-03-06 17:16:33 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+checkout_url = "http://google.com"
+st.link_button(
+    label=":black[Click to Checkout]",
+    url=checkout_url,
+    help="Click to visit check-out page to complete payment. Checkout will expire in 3 minutes.",
+    type="secondary",
+    use_container_width=False
+)
