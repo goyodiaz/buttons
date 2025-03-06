37d8de759789320a3e843e939c1fb61c65055605
Goyo <goyodiaz@gmail.com>
2025-03-06 17:52:47 +0100

    Add sidebar button.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,14 @@ import streamlit as st
 
 checkout_url = "http://google.com"
 st.link_button(
-    label=":black[Click to Checkout]",
-    url=checkout_url,
-    help="Click to visit check-out page to complete payment. Checkout will expire in 3 minutes.",
-    type="secondary",
-    use_container_width=False
-)
-st.link_button(
     label="Click to Checkout",
     url=checkout_url,
     help="Click to visit check-out page to complete payment. Checkout will expire in 3 minutes.",
     type="secondary",
     use_container_width=False
 )
-st.link_button(
-    label="**Click to Checkout**",
+st.sidebar.link_button(
+    label="Click to Checkout",
     url=checkout_url,
     help="Click to visit check-out page to complete payment. Checkout will expire in 3 minutes.",
     type="secondary",
